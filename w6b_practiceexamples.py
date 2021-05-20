9ee32722dbe4115bc6f5697df135148ab3da0e26
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-05-20 11:30:16 +0530

    Create w6b_practiceexamples.py

---
+++ w6b_practiceexamples.py
@@ -0,0 +1,72 @@
+# Day to number dictionary problem
+
+# Student should enter code below
+
+day_to_number = {'Sunday': 0,"Monday" : 1, "Tuesday": 2,"Wednesday":3,"Thursday":4,"Friday":5,"Saturday":6}
+
+# Test data
+
+print day_to_number["Sunday"]
+print day_to_number["Monday"]
+print day_to_number["Tuesday"]
+print day_to_number["Wednesday"]
+print day_to_number["Thursday"]
+print day_to_number["Friday"]
+print day_to_number["Saturday"]
+
+# Sample output
+
+#0
+#1
+#2
+#3
+#4
+#5
+#6
+
+###################################################
+# Day to number dictionary problem
+
+# Student should enter code below
+name_lookup={"Joe":'Warren',"Scott":'Rixner',"John":'Greiner',"Stephen":'Wong'}
+
+# Test data
+
+print name_lookup["Joe"]
+print name_lookup["Scott"]
+print name_lookup["John"]
+print name_lookup["Stephen"]
+
+
+# Sample output
+
+#Warren
+#Rixner
+#Greiner
+#Wong
+
+###################################################
+
+# Image debugging problem
+
+# Student should enter code below
+
+import simplegui
+
+# load test image
+test_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/Race-Car.png")
+test_image_size = [135, 164]
+test_image_center = [test_image_size[0] / 2, test_image_size[1] / 2]
+
+# draw handler
+def draw(canvas):
+    canvas.draw_image(test_image,test_image_center,test_image_size,test_image_center,test_image_size)
+
+# create frame and register draw handler    
+frame = simplegui.create_frame("Test image", test_image_size[0], test_image_size[1])
+frame.set_draw_handler(draw)
+
+# start frame
+frame.start()
+        
+                                       
