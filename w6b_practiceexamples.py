57e15127a905f5a94a8b948b5da0a5184f2bee3a
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2021-05-20 12:03:47 +0530

    Update w6b_practiceexamples.py

--- w6b_practiceexamples.py
+++ w6b_practiceexamples.py
@@ -69,4 +69,43 @@ frame.set_draw_handler(draw)
 # start frame
 frame.start()
         
+###################################################
+# Image positioning problem
+
+# Student should enter code below
+
+import simplegui
+
+# global constants
+WIDTH = 400
+HEIGHT = 300
+
+# load test image
+test_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/asteroid.png")
+test_image_size = [95, 93]
+test_image_center = [test_image_size[0] / 2, test_image_size[1] / 2]
+test_image_pos=test_image_center
+
+
+
+# mouseclick handler
+def click(pos):
+    global test_image_pos
+    test_image_pos=pos
+    print test_image_pos
+    
+# draw handler
+def draw(canvas):
+    global test_image_pos
+    canvas.draw_image(test_image,test_image_center,test_image_size,test_image_pos,test_image_size)
+
+# create frame and register draw handler    
+frame = simplegui.create_frame("Test image", WIDTH, HEIGHT)
+frame.set_canvas_background("Gray")
+frame.set_draw_handler(draw)
+frame.set_mouseclick_handler(click)
+
+# start frame
+frame.start()
+        
                                        
